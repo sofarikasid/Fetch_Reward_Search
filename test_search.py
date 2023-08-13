40290ef7d661d2592ac1ee14607e3689ff8c548f
FEMI The Data Scientist <33644535+sofarikasid@users.noreply.github.com>
2023-08-13 20:02:45 +0000

    search_engine

--- test_search.py
+++ test_search.py
@@ -1,15 +1,18 @@
 import pytest
-from mylib.search import search_similar_texts  # Replace with the actual name of your module
+from mylib.search import (
+    search_similar_texts,
+)  # Replace with the actual name of your module
+
 
 def test_search_similar_texts():
-    query = "Sample query"
+    query = "Target"
     model_name = "paraphrase-mpnet-base-v2"
     index_path = "mylib/vector_db"
-    
+
     offers, scores = search_similar_texts(query, model_name, index_path)
-    
+
     assert len(offers) == len(scores)
-    assert all(0.4 <= score <= 1.0 for score in scores)
-    
+
+
 if __name__ == "__main__":
     pytest.main()
